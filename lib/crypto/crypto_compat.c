685561f1e8e9530f8cc1127f662c91b1bd5c50f8
Graham Percival <gperciva@tarsnap.com>
2017-01-18 19:26:15 -0800

    crypto_compat: add crypto_compat_RSA_import()
    This commit shuffles some code from crypto_rsa.c into crypto_compat.
    
    crypto_compat_RSA_import() is not yet compatible with OpenSSL 1.1.

--- lib/crypto/crypto_compat.c
+++ lib/crypto/crypto_compat.c
@@ -26,3 +26,37 @@ err0:
 	/* Failure! */
 	return (-1);
 }
+
+/**
+ * crypto_compat_RSA_import(key, n, e, d, p, q, dmp1, dmq1, iqmp):
+ * Import the given BIGNUMs into the RSA ${key}.
+ */
+int
+crypto_compat_RSA_import(RSA ** key, BIGNUM * n, BIGNUM * e, BIGNUM * d,
+    BIGNUM * p, BIGNUM * q, BIGNUM * dmp1, BIGNUM * dmq1, BIGNUM * iqmp)
+{
+
+	/* Sanity checks. */
+	assert(key != NULL);
+	assert((n != NULL) && (e != NULL));
+	/* All the private-key-related variables are NULL, or they're not. */
+	assert( ((d == NULL) && (p == NULL) && (q == NULL) && (dmp1 == NULL)
+	      && (dmq1 == NULL) && (iqmp == NULL)) ||
+	        ((d != NULL) && (p != NULL) && (q != NULL) && (dmp1 != NULL)
+	      && (dmq1 != NULL) && (iqmp != NULL)));
+
+	/* Put values into RSA key. */
+	(*key)->n = n;
+	(*key)->e = e;
+	if (d != NULL) {
+		(*key)->d = d;
+		(*key)->p = p;
+		(*key)->q = q;
+		(*key)->dmp1 = dmp1;
+		(*key)->dmq1 = dmq1;
+		(*key)->iqmp = iqmp;
+	}
+
+	/* Success! */
+	return (0);
+}
